84a0c72e2d7aade51d3f2d25a02536cdc673d92e
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-02-21 15:16:35 +0000

    init

---
+++ auctions/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False)
---
+++ auctions/views.py
@@ -0,0 +1,89 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import User, Listing
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.http import JsonResponse
+import ast
+
+def index(request):
+    return render(request, "auctions/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "auctions/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "auctions/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "auctions/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "auctions/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "auctions/register.html")
+
+def newListingPage(request):
+    if request.user.is_authenticated:
+        return render(request, 'auctions/new.html')
+    else: 
+        return redirect('/login')
+
+@csrf_exempt
+def createListing(request): 
+    if request.user.is_authenticated:
+        requestData = ast.literal_eval(request.body.decode('utf8'))
+        newListing = Listing(title=requestData["title"])
+        newListing.save()
+        payload = {
+            'status': 'success',
+            'requestBody' : requestData
+        }
+        return JsonResponse(payload)
+    else:
+        payload = {
+            'status': 'failure: not logged in',
+        }
+        return JsonResponse(payload, status=401)
+        
